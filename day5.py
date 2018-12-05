75922ffaa819b06f0141ee38e6558d2d36a4bbf4
Alpvax <development@alpvax.uk>
2018-12-05 09:22:59 +0000

    Add day 5

---
+++ day5.py
@@ -0,0 +1,23 @@
+from loadFile import loadfile
+
+a = ord("a")
+A = ord("A")
+
+def reactedLength(data):
+  last = None
+  while last != data:
+    last = data
+    for i in range(26):
+      data = data.replace(chr(a + i) + chr(A + i),"")
+      data = data.replace(chr(A + i) + chr(a + i),"")
+  return len(data)
+
+if __name__ == "__main__":
+  data = list(loadfile(5))[0][0:-1] # Strip newline character :@
+
+  print(reactedLength(data))
+
+  best = len(data)
+  for i in range(26):
+     best = min(best, reactedLength(data.replace(chr(a + i), "").replace(chr(A + i), "")))
+  print(best)
