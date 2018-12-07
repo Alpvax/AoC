023087dcbf8d732e81dd30b9c53e4db791892cb0
Alpvax <alpvax@gmail.com>
2018-12-07 16:14:44 +0000

    Finish day 7

--- day7.py
+++ day7.py
@@ -1,21 +1,26 @@
 from loadFile import loadfile
-from collections import Counter
+from functools import total_ordering
 import re
 
 class NodeMeta(type):
-  def __call__(cls, id, *args, **kwarg):
-    if id in cls.ids:
-      return cls.ids[id]
-    n = super().__call__(id, *args, **kwarg)
-    cls.ids[id] = n
+  def __call__(cls, key, *args, **kwarg):
+    if key in cls.items:
+      return cls.items[key]
+    n = super().__call__(key, *args, **kwarg)
+    cls.items[key] = n
     return n
 
+@total_ordering
 class Node(metaclass=NodeMeta):
-  ids = {}
-  def __init__(self, id):
-    self.id = id
+  items = {}
+  order = {}
+  rules = []
+  def __init__(self, key):
+    self.key = key
     self.children = set()
     self.parents = set()
+    self.duration = ord(self.key) - 4 # (60 - 65 + 1) (ord("A") = 65)
+    self.sortIndex = key
   def addChild(self, child):
     if type(child) != Node:
       child = Node(child)
@@ -27,16 +32,56 @@ class Node(metaclass=NodeMeta):
   def __iter__(self):
     yield from sorted(self.children)
   def __str__(self):
-    return "Node:{}{}".format(self.id, sorted([n.id for n in self.children]))
+    return "Node:{}{}".format(self.key, sorted([n.key for n in self.children]))
   def __repr__(self):
     return str(self)
+  def __hash__(self):
+    return hash(self.key)
+  def __eq__(self, other):
+    if hasattr(other, "key"):
+      return self.key == other.key
+    return NotImplemented
+  def __lt__(self, other):
+    if hasattr(other, "sortIndex"):
+      return self.sortIndex < other.sortIndex
+    return NotImplemented
 
-def nodeRecurse(node, index, callback):
-  print(index, node)
-  index = callback(node, index)
-  for n in node.children:
-    index = nodeRecurse(n, index, callback)
-  return index
+def sortNodes():
+  sortedNodes = []
+  nodes = set(node for node in Node.items.values() if node.root)
+  while len(nodes) > 0:
+    for node in sorted(nodes, key=lambda n: n.key):
+      if len([p for p in node.parents if p.key not in sortedNodes]) < 1: # If parents are all added
+        node.sortIndex = len(sortedNodes)
+        sortedNodes.append(node.key)
+        nodes.remove(node)
+        nodes.update(node.children)
+        break
+  return sortedNodes
+
+def build(order, numWorkers = 4 + 1):
+  remaining = set(Node(k) for k in order)
+  current = {}
+  sec = 0
+  while len(remaining) > 0:
+    if len(current) < numWorkers:
+      available = [node for node in sorted(remaining) if node not in current and len(remaining.intersection(node.parents)) < 1][0: numWorkers - len(current)]
+#      print(current, available)
+      for node in available:
+        current[node] = node.duration
+#      print(current)
+    step = min(current.values())
+    sec += step
+#    print("Currently building: {}\nSkipping {} secs, total time ellapsed: {}".format({n.key: d for n,d in current.items()}, step, sec))
+    for n,d in dict(current).items():
+      d -= step
+      if d < 1:
+        del current[n]
+        remaining.remove(n)
+      else:
+        current[n] = d
+  return sec
+
 
 if __name__ == "__main__":
   pattern = re.compile(r"^Step ([A-Z]) .+ step ([A-Z])")
@@ -44,13 +89,7 @@ if __name__ == "__main__":
     m = pattern.match(line)
     Node(m[1]).addChild(m[2])
 
-  nodeOrder = {}
-  def setNodePos(node, index):
-    nodeOrder[node.id] = index
-    return  index + 1
+  order = sortNodes()
+  print("Sorted order:", "".join(order))
 
-  i = 0
-  for node in sorted(Node.ids.values(), key=lambda n: n.id):
-    if node.root:
-      i += nodeRecurse(node, i, setNodePos)
-  print("".join(reversed([id[0] for id in Counter(nodeOrder).most_common()])))
+  print("Total build time:", build(order))
