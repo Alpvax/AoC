a203f8a0ab7c71f9c6d47eeffb0285cc2f0c7aaf
Alpvax <development@alpvax.uk>
2018-12-08 17:50:14 +0000

    Reduced day 8 to 10 lines because I can

--- day8compact.py
+++ day8compact.py
@@ -1,12 +1,10 @@
 class Node():
-  nodes = []
   def __init__(self, data):
     h = [data.popleft(), data.popleft()]
     self.children = [Node(data) for i in range(h[0])]
     self.meta = [data.popleft() for i in range(h[1])]
-    Node.nodes.append(self)
-  def value(self):
-    return sum(self.meta) if len(self.children) < 1 else sum(self.children[m - 1].value() for m in self.meta if m <= len(self.children))
+  def v1(self): return sum(self.meta) + sum(c.v1() for c in self.children)
+  def value(self): return sum(self.meta) if len(self.children) < 1 else sum(self.children[m - 1].value() for m in self.meta if m <= len(self.children))
 
 import loadFile, collections
-print("Sum of all metadata: {p1}\nValue of root node: {p2}".format(p2 = Node(collections.deque(map(int, next(loadFile.loadfile(8)).split(" ")))).value(), p1 = sum(sum(n.meta) for n in Node.nodes)))
+print("Sum of all metadata: {}\nValue of root node: {}".format(*[getattr(Node(collections.deque(map(int, next(loadFile.loadfile(8)).split(" ")))), m)() for m in ["v1", "value"]]))
