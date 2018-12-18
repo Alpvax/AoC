c8e6227e329999cefa5a52024ce2e191bc8de259
Alpvax <alpvax@gmail.com>
2018-12-18 18:27:45 +0000

    Complete day 17 part 1 with sample data
    Hits recursion limit with real data

--- day17.py
+++ day17.py
@@ -15,7 +15,9 @@ def spread(x, y):
   if y + 1 > Y_MAX:
     setTile(x, y, "|")
     print(f"Hit the bottom at ({x},{y})")
+    return
   if getTile(x, y + 1) in ".|":
+    setTile(x, y, "|")
     spread(x, y + 1)
   if getTile(x, y + 1) in "~#":
     left = x
@@ -24,7 +26,21 @@ def spread(x, y):
     right = x
     while getTile(right, y) in ".|" and getTile(right, y + 1) in "~#":
       right += 1
-    print(f"{left} - {right} @ {y}:", [getTile(i, y) for i in range(left, right + 1)])
+    row = [getTile(i, y) for i in range(left, right + 1)]
+    tile = "|"
+    def gapsBelow(left, right, y):
+      return [left + i for i, t in enumerate(getTile(i, y + 1) for i in range(left, right + 1)) if t in ".|"]
+    if len(gapsBelow(left, right, y)) < 1:#row[0] == "#" and row [-1] == "#":
+      tile = "~"
+    else:
+      for gapx in gapsBelow(left, right, y):
+        spread(gapx, y + 1)
+      if len(gapsBelow(left, right, y)) < 1: # Repeat spread if gaps are now filled
+        spread(x, y)
+    for i in range(left, right + 1): # Fill row
+      if getTile(i, y) in ".|":
+        setTile(i, y, tile)
+    print(f"\n{left} - {right} @ {y}:", row)
     print(f"{left} - {right} @ {y + 1}:", [getTile(i, y + 1) for i in range(left, right + 1)])
 
 def printTiles():
@@ -38,7 +54,7 @@ if __name__ == "__main__":
   reval = r"(\d+)(?:\.{2}(\d+))?"
   px = re.compile(r"x=" + reval)
   py = re.compile(r"y=" + reval)
-  for line in loadfile(17, sample=True):
+  for line in loadfile(17, sample=False):
     xr = px.search(line)
     xlim = [int(xr[1]), int(xr[2]) + 1 if xr[2] != None else int(xr[1]) + 1]
     yr = py.search(line)
@@ -48,17 +64,10 @@ if __name__ == "__main__":
     for x in range(xlim[0], xlim[1]):
       for y in range(ylim[0], ylim[1]):
         setTile(x, y, "#")
-  printTiles()
+  #printTiles()
 
   x = 500
   y = 0
   spread(x, y + 1)
-#  while y < area[1][1]:
-#    tile = getTile(x, y)
-#    below = getTile(x, y + 1)
-#    if below in ["sand", "wet_sand"]:
-#      y += 1
-#      continue
-#    else:
-#      print(f"Spreading above {below} at ({x}, {y})")
-#      break
+  print(len([t for t in TILES if t in "~|"]))
+  #printTiles()
