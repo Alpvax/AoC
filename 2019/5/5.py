e86515384e87e39b30f69d11e81f62fd31477ba5
Alpvax <development@alpvax.uk>
2019-12-06 12:10:57 +0000

    Finish 2019/5

--- 2019/5/5.py
+++ 2019/5/5.py
@@ -1,38 +1,53 @@
 #! /bin/python3
 
-#with open("input.txt") as f:
-#  inputData = [int(n) for n in f.read().split(",")]
+if True:
+  with open("input.txt") as f:
+    inputData = [int(n) for n in f.read().split(",")]
 #inputData = [1,9,10,3,2,3,11,0,99,30,40,50]
-inputData = [1002,4,3,4,33]
+#inputData = [1002,4,3,4,33]
+#inputData = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+#inputData = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
 
-operations = {
-  1: (2, lambda args: sum(args)),
-  2: (2, lambda args: args[0] * args[1]),
-  3: (1, lambda args: args[0]),
+operations = { # (numArgs, (args, currentIndex) -> (result, newIndex))
+  1: (2, lambda args, i: (sum(args), i + 4)),                               # 1 - Sum
+  2: (2, lambda args, i: (args[0] * args[1], i + 4)),                       # 2 - Product
+  3: (0, lambda args, i: (int(input("Enter the input number: ")), i + 2)),  # 3 - Input
+  4: (1, lambda args, i: (print("Output:", args[0]), i + 2)),               # 4 - Output
+  5: (2, lambda args, i: (None, (i + 3) if not args[0] else args[1])),      # 5 - JumpIfTrue
+  6: (2, lambda args, i: (None, (i + 3) if args[0] else args[1])),          # 6 - JumpIfFalse
+  7: (2, lambda args, i: (1 if args[0] < args[1] else 0, i + 4)),           # 7 - LessThan
+  8: (2, lambda args, i: (1 if args[0] == args[1] else 0, i + 4)),          # 8 - Equals
 }
 
 def getValue(iData, index, immediate):
-  print(index, "->", iData[index], immediate)
-  return iData[index] if immediate else iData[iData[index]]
+  data = iData[index]
+  if immediate:
+    return data
+  return iData[data]
 
-def run(inputArg):
+def run():
   iData = inputData.copy()
-  iData[1] = inputArg
   i = 0
+  #print([n for n in range(len(iData))])
   while i < len(iData):
+    #print(iData)
     code = iData[i]
     op = code % 100
     immediate = "{:03d}".format((code // 100))
-    print(immediate, [(j, immediate[-1 -j]) for j in range(3)])
-    print()
     if op == 99:
       break
     else:
-      print(op, operations.get(op, "ERROR: Invalid op (" + str(i) + ")"))
-      numArgs,oper = operations.get(op, "ERROR: Invalid op (" + str(i) + ")")
-      args = [getValue(iData, i+j, immediate[-1 - j] == "1") for j in range(numArgs)]
-      iData[iData[i + numArgs + 1]] = oper(args)
-      i += numArgs + 2
+      #print(i, code)
+      numArgs,oper = operations.get(op, (0, lambda args, index: (_ for _ in ()).throw(Exception("Invalid op at index {} ({})".format(i, code)))))
+      args = [getValue(iData, i+j+1, immediate[-1 - j] == "1") for j in range(numArgs)]
+      #print(i, op, args)
+      res, nextI = oper(args, i)
+      if res != None:
+        iData[iData[i + numArgs + 1]] = res
+        #i += 1
+        #print("iData[{} -> {}] = {}".format(i + numArgs + 1, iData[i + numArgs + 1], res))
+      #i += numArgs + 1
+      i = nextI
   return iData[0]
 
-print(run(1))
+run()
